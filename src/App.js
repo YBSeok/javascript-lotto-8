b2fdfc7875f334fdde5f326d9191aeefca86cfbc
YBSeok <soberyl4304@gmail.com>
2025-11-03 23:25:49 +0900

    feat: add run program logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import InputManager from "./InputManager.js";
+import OutputManager from "./OutputManager.js";
+import LottoMachine from "./LottoMachine.js";
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  #lottoMachine;
+
+  constructor() {
+    this.#lottoMachine = new LottoMachine();
+  }
+
+  async run() {
+    try {
+      const purchaseAmount = await InputManager.getPurchaseAmount();
+      const lottos = this.#lottoMachine.issueLottos(purchaseAmount);
+      OutputManager.printPurchasedLottos(lottos);
+
+      const winningLotto = await InputManager.getWinningLotto();
+      const statistics = this.#lottoMachine.calculateStatistics(
+        lottos,
+        winningLotto
+      );
+
+      const totalReturnRate = this.#lottoMachine.calculateTotalReturnRate(
+        purchaseAmount,
+        statistics
+      );
+
+      OutputManager.printStatistics(statistics, totalReturnRate);
+    } catch (error) {
+    } finally {
+      Console.close();
+    }
+  }
 }
 
 export default App;
