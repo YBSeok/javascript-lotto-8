34e4d4fd62c3ad0372163842c35ad2ebcd6081e6
YBSeok <soberyl4304@gmail.com>
2025-11-03 23:40:26 +0900

    feat: add exception in lotto

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -1,45 +1,18 @@
-import { ERROR, LOTTO } from "../src/Constants.js";
+import Lotto from "../src/Lotto";
 
-class Lotto {
-  #numbers;
+describe("로또 클래스 테스트", () => {
+  test("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6, 7]);
+    }).toThrow("[ERROR]");
+  });
 
-  constructor(numbers) {
-    this.#validate(numbers);
-    this.#numbers = numbers;
-  }
+  // TODO: 테스트가 통과하도록 프로덕션 코드 구현
+  test("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 5]);
+    }).toThrow("[ERROR]");
+  });
 
-  #validate(numbers) {
-    if (numbers.length !== LOTTO.NUMBER_COUNT) {
-      throw new Error(ERROR.NUMBER_COUNT);
-    }
-
-    const uniqueNumbers = new Set(numbers);
-    if (uniqueNumbers.size !== numbers.length) {
-      throw new Error(ERROR.NUMBER_DUPLICATE);
-    }
-
-    numbers.forEach((number) => {
-      this.#validateNumberRange(number);
-    });
-  }
-
-  #validateNumberRange(number) {
-    if (
-      !Number.isInteger(number) ||
-      number < LOTTO.MIN_NUMBER ||
-      number > LOTTO.MAX_NUMBER
-    ) {
-      throw new Error(ERROR.NUMBER_RANGE);
-    }
-  }
-
-  getNumbers() {
-    return this.#numbers.slice().sort((a, b) => a - b);
-  }
-
-  contains(number) {
-    return this.#numbers.includes(number);
-  }
-}
-
-export default Lotto;
+  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+});
